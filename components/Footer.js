5c4c3752d3ab6f2f41940e8946af79b6843fa225
inntenz <141833758+inntenz@users.noreply.github.com>
2024-12-03 09:48:51 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <img src="/intenz.png" alt="my Logo" className={styles.logo} />
       </footer>
     </>
   )
